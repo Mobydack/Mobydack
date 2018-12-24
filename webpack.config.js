f4adc53aa86b69df08be2e2e224b954c096a31dd
Mobydack <devzetsubou@gmail.com>
2018-12-24 04:03:05 +0300

    Added packages for working with styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const MiniCssExtractPlugin = reuire('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const devMode = process.env.NODE_ENV === 'production';
 
 module.exports = {
     entry: {
         app: './src/app.js'
     },
     resolve: {
-        extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx'],
+        extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx', '.styl'],
         alias: {
             components: path.resolve(__dirname, 'src/components'),
             compound: path.resolve(__dirname, 'src/components/compound'),
@@ -21,12 +23,29 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: 'babel-loader'
+            },
+            //style
+            {
+                test: /\.(styl)$/,
+                exclude: /node_modules/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin,
+                    'css-loader',
+                    'postcss-loader',
+                    'stylus-laoder'
+                ]
             }
         ]
     },
     plugins: [
+        //html template
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }),
+        // style file
+        new MiniCssExtractPlugin({
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunlFilename: devMode ? '[id].css' : '[id].[hash].css',
         }),
     ],
     output: {
